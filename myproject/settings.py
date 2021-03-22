31da02c0cdc624b7eeec2979d646d63a2c3700f5
Swati228 <swatiyunoki22@gmail.com>
2021-03-22 20:37:07 +0530

    Deploymnt steps 2 confugure db & middleware

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ij0+ey1r0w4*i6up$xuuw%=f^a0jlt7bt4x95zu#1r$52t@u5('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -88,15 +89,17 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER':'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'mydb',
+#        'USER':'postgres',
+#        'PASSWORD':'1234',
+#        'HOST':'localhost',
+#    }
+#}
+
+DATABASES ={'default': dj_database_url.config(default='postgres://postgres:1234@localhost/mydb')}
 
 
 # Password validation
@@ -158,6 +161,12 @@ MESSAGE_TAGS = {
 }
 
 SITE_ID = 1
+
+#whitenoise static
+
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT =587
